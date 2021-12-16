b419dea09a1f47ab018283ce8c07207ef6e949c9
diofajrie <diofajriefadlullah17@gmail.com>
2021-12-16 15:41:17 +0700

    login with jetstream

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,7 @@ Route::get('/', [PagesController::class, 'home']);
 Route::get('/eror', [PagesController::class, 'eror']);
 Route::get('/contact', [PagesController::class, 'contact']);
 Route::get('/team', [PagesController::class, 'team']);
+// Route::get('/login', [PagesController::class, 'login']);
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
